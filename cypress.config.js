72c9b3703b923704097bac953c1b141c88956306
Guilherme Pinheiro <guilhermepinheiro.silva@hotmail.com>
2026-03-28 15:58:43 +0900

    git headlss e relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -8,5 +8,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true, // Enable recording  
+    videoCompression: 25, // Higher compression (smaller files)  
+    videoUploadOnPasses: true, // Save videos for passing tests  
+    videoFolder: 'tests/e2e-videos', // Custom storage path
   },
 });
